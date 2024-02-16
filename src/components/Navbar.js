32983f484e595b8da38b8465f1f476530b0b27cd
Esmer Quliyeva <139906367+EsmerQuliyeva@users.noreply.github.com>
2024-02-16 13:35:24 -0800

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+
+
+function App() {
+ 
+
+ 
+
+  return (
+    <div className="App">
+      <nav className="navbar">
+        <div className="navbar-container">
+          <h1 className="logo">Ask Experts</h1>
+          <ul className="nav-links">
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/service'>Service</Link></li>
+            <li><Link to='/about'>Contacts</Link></li>
+            <li><Link to='/about'>Our team</Link></li>
+            <li><Link to='/service'>Our Approach</Link></li>
+            <li><Link to='/about'>020-230-45-67</Link></li>
+            {/* <li className='number'><a href="#">575-33-44</a></li> */}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
